b6e0d355732386cdb17a0a78b56ed0046f822989
Syed Razvi <razvisyed2002@gmail.com>
2025-07-26 16:39:27 +0530

    Edited the Dashboard and added Tasks

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 from app.models import db
 from app.config import Config
 from app.views.auth import auth_bp
-
+from app.views.dashboard import dashboard_bp
 migrate = Migrate()
 
 def create_app():
@@ -14,6 +14,7 @@ def create_app():
     migrate.init_app(app, db)
 
     app.register_blueprint(auth_bp)
+    app.register_blueprint(dashboard_bp)
 
     @app.route('/')
     def root_redirect():
